44c53680f609f77e2c0c06c924f910fe52b20f4c
nathangero <nathangero@yahoo.com>
2023-10-19 17:29:12 -0700

    fix switch case to be lower case; add testing args

--- index.js
+++ index.js
@@ -71,24 +71,23 @@ function validateColor(color) {
 
 async function askUser() {
     let { name, textColor, shape, shapeColor } = await inquirer.prompt(questions);
-    console.log(name, textColor, shape, shapeColor);
-
     let logo;
 
     switch (shape) {
-        case "Circle":
+        case "circle":
             logo = new Circle(name, textColor, shapeColor);
             break;
 
-        case "Square":
+        case "square":
             logo = new Square(name, textColor, shapeColor);
             break;
         
-        case "Triangle":
+        case "triangle":
             logo = new Triangle(name, textColor, shapeColor);
             break;
 
         default: // This shouldn't run but just in case
+            console.log("default");
             logo = new Shape(name, textColor, shapeColor);
     }
 
@@ -99,4 +98,37 @@ async function askUser() {
     })
 }
 
-askUser()
+// For testing purposes
+if (process.argv[2]) {
+    let name = "nag";
+    let textColor = "red";
+    let shapeColor = "black";
+    switch (process.argv[2]) {
+        case "circle":
+            console.log("circle");
+            logo = new Circle(name, textColor, shapeColor);
+            break;
+
+        case "square":
+            console.log("square");
+            logo = new Square(name, textColor, shapeColor);
+            break;
+        
+        case "triangle":
+            console.log("triangle");
+            logo = new Triangle(name, textColor, shapeColor);
+            break;
+
+        default: // This shouldn't run but just in case
+            console.log("default");
+            logo = new Shape(name, textColor, shapeColor);
+    }
+
+    // console.log("logo:", logo.render());
+
+    fs.writeFile("./logo.svg", logo.render(), err => {
+        err ? console.log(err) : console.log("Generated logo.svg")
+    })
+} else {
+    askUser()
+}
