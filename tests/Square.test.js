8b0f4c78ddb9d457e45ad61bf5569e6b6d2715a1
nathangero <nathangero@yahoo.com>
2023-10-21 21:23:41 -0700

    create tests for all shapes

---
+++ tests/Square.test.js
@@ -0,0 +1,14 @@
+const Square = require("../lib/Square");
+
+describe("Square", () => {
+    it("should return a valid <rect> and <text> inside of a <svg> tag", () => {
+        const square = new Square("abc", "white", "black");
+        const result =
+        `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">\n` +
+        `   <rect width="200" height="200" fill="black"></rect>\n` +
+        `   <text x="75" y="110" font-family="helvetica" font-size="30" fill="white">abc</text>\n` +
+        `</svg>`
+
+        expect(square.render()).toBe(result);
+    })
+})
